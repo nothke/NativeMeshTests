7bd7d05e31c54116d2624a7cc29b35301220344d
Ivan Notaros <nothke@gmail.com>
2019-09-17 03:00:21 +0200

    Custom vertex struct test

---
+++ Assets/Scripts/Vertex.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.Mathematics;
+
+[System.Runtime.InteropServices.StructLayout(System.Runtime.InteropServices.LayoutKind.Sequential)]
+struct Vertex
+{
+    public float3 pos;
+    public float3 normal;
+}
